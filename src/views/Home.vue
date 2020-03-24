8d37e4f1a4f54f5ee580c00516c3bc8c34edbae4
unknown <61458694+fengzhuo-FZ@users.noreply.github.com>
2020-03-24 14:50:17 +0800

    首页模块-基础布局

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="home-container">首页</div>
+  <!-- 全屏容器 -->
+  <el-container class="home-container">
+    <!-- 侧边栏 -->
+    <el-aside class="my-aside" width="200px">Aside</el-aside>
+    <!-- 右侧主体 -->
+    <el-container>
+      <!-- 头部内容 -->
+      <el-header class="my-header">Header</el-header>
+      <!-- 主体内容 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -8,4 +19,18 @@ export default {
 };
 </script>
 
-<style scoped lang='less'></style>
+<style scoped lang='less'>
+.home-container {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  .my-aside {
+    background: #002244;
+  }
+  .my-header {
+    border-bottom: 1px solid #ddd;
+  }
+}
+</style>
