99ba6fbc818e2d184e7ca56ee82ebda206e7e128
unknown <61458694+fengzhuo-FZ@users.noreply.github.com>
2020-03-27 15:22:13 +0800

    内容管理-请求拦截器

--- src/api/index.js
+++ src/api/index.js
@@ -7,6 +7,16 @@ import auth from '@/utils/auth'
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
 
 //请求头 配置token
-axios.defaults.headers.Authorization = `Bearer ${auth.getUser().token}`
+// axios.defaults.headers.Authorization = `Bearer ${auth.getUser().token}`
+
+// 请求拦截器
+axios.interceptors.request.use(config => {
+    // 当前函数：在每次请求前都会执行。
+    // 修改请求头配置，追加上token
+    // 做一个严谨的判断，本地存储了token就是追加
+    const { token } = auth.getUser()
+    if (token) config.headers.Authorization = `Bearer ${token}`
+    return config
+}, err => Promise.reject(err))
 
 export default axios
