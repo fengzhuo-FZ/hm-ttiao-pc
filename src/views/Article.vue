5775fc47d0995f4232b3b9133e3839c03489718f
unknown <61458694+fengzhuo-FZ@users.noreply.github.com>
2020-03-29 15:34:10 +0800

    内容管理-表格渲染

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -19,7 +19,12 @@
           </el-radio-group>
         </el-form-item>
         <el-form-item label="频道：">
-          <el-select v-model="reqParams.channel_id" placeholder="请选择">
+          <el-select
+            @change="changeChannel"
+            clearable
+            v-model="reqParams.channel_id"
+            placeholder="请选择"
+          >
             <el-option
               v-for="item in channelOptions"
               :key="item.id"
@@ -35,26 +40,57 @@
             range-separator="至"
             start-placeholder="开始日期"
             end-placeholder="结束日期"
+            change="changeDate"
+            value-format="yyyy-MM-dd"
           ></el-date-picker>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary">筛选</el-button>
+          <el-button type="primary" @click="filterArticle()">筛选</el-button>
         </el-form-item>
       </el-form>
     </el-card>
     <!-- 筛选结果区域 -->
     <el-card style="margin-top:20px">
-      <div slot="header">根据筛选条件共查询到 0 条结果：</div>
+      <div slot="header">根据筛选条件共查询到 {{total}} 条结果：</div>
       <!-- 表格 -->
       <el-table :data="articles">
-        <el-table-column label="封面"></el-table-column>
-        <el-table-column label="标题"></el-table-column>
-        <el-table-column label="状态"></el-table-column>
-        <el-table-column label="发布时间"></el-table-column>
-        <el-table-column label="操作"></el-table-column>
+        <el-table-column label="封面">
+          <template slot-scope="scope">
+            <el-image fit="cover" :src="scope.row.cover.images[0]" style="width:150px;height:100px">
+              <div slot="error" class="image-slot">
+                <img src="../assets/error.gif" style="width:150px;height:100px" />
+              </div>
+            </el-image>
+          </template>
+        </el-table-column>
+        <el-table-column label="标题" prop="title"></el-table-column>
+        <el-table-column label="状态">
+          <template slot-scope="scope">
+            <el-tag v-if="scope.row.status===0" type="info">草稿</el-tag>
+            <el-tag v-if="scope.row.status===1">待审核</el-tag>
+            <el-tag v-if="scope.row.status===2" type="success">审核通过</el-tag>
+            <el-tag v-if="scope.row.status===3" type="warning">审核失败</el-tag>
+            <el-tag v-if="scope.row.status===4" type="danger">已删除</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="发布时间" prop="pubdate"></el-table-column>
+        <el-table-column label="操作" width="120px">
+          <template>
+            <el-button type="primary" icon="el-icon-edit" circle plain></el-button>
+            <el-button type="danger" icon="el-icon-delete" circle plain></el-button>
+          </template>
+        </el-table-column>
       </el-table>
       <!-- 分页 -->
-      <el-pagination style="margin-top:30px" background layout="prev, pager, next" :total="1000"></el-pagination>
+      <el-pagination
+        style="margin-top:20px"
+        background
+        layout="prev, pager, next"
+        :page-size="reqParams.per_page"
+        :current-page="reqParams.page"
+        :total="total"
+        @current-change="changePager"
+      ></el-pagination>
     </el-card>
   </div>
 </template>
@@ -70,7 +106,9 @@ export default {
         status: null,
         channel_id: null,
         begin_pubdate: null,
-        end_pubdate: null
+        end_pubdate: null,
+        page: 1, // 默认第一页
+        per_page: 10 // 一页显示10条
       },
       // 频道下拉选项数据
       channelOptions: [],
@@ -78,13 +116,58 @@ export default {
       // 当日期控件选择了日期后动态给 reqParams 中 begin_pubdate end_pubdate 赋值
       dateArr: [],
       // 文章列表
-      articles: []
+      articles: [],
+      total: 0
     };
   },
   created() {
     this.getChannelOptions();
+    this.getArticles();
   },
   methods: {
+    // 选择频道
+    changeChannel(value) {
+      // 清空的时候值是 '' 不符合后台要求，你应该改成 null  代表查询全部
+      if (value === "") this.reqParams.channel_id = null;
+    },
+    // 筛选文章
+    filterArticle() {
+      // 回到第一页
+      this.reqParams.page = 1;
+      // 根据reqParams进行进行查处
+      this.getArticles();
+    },
+    // 改变日期
+    changeDate(dateArr) {
+      // 根据选择的时间，给起始和结束数据赋值。
+      // 现在日期格式是标准日期格式 Date 格式。
+      // 后台接口要是 字符串格式 的日期  例如： `2020-03-01`
+      // 在给 起始和结束数据赋值前  转换格式为 字符串日期格式 value-format="yyyy-MM-dd"
+      // 如果 执行清空日期操作  dateArr === null
+      if (dateArr) {
+        this.reqParams.begin_pubdate = dateArr[0];
+        this.reqParams.end_pubdate = dateArr[1];
+      } else {
+        this.reqParams.begin_pubdate = null;
+        this.reqParams.end_pubdate = null;
+      }
+    },
+    // 进行分页
+    changePager(newPage) {
+      // 根据新的页码，重新获取列表数据即可，进行渲染
+      this.reqParams.page = newPage;
+      this.getArticles();
+    },
+    // 获取文章列表数据
+    async getArticles() {
+      const {
+        data: { data }
+      } = await this.$http.get("articles", { params: this.reqParams });
+      this.articles = data.results;
+      // 总条数
+      this.total = data.total_count;
+    },
+
     // 获取频道下拉选项数据
     async getChannelOptions() {
       // 解构是针对res的，那么res的数据结构 {data:{message:'OK',data:{channels:[]}}}
