d56e87f140d0891a6549aac9aa4f7d91ee360249
unknown <61458694+fengzhuo-FZ@users.noreply.github.com>
2020-04-02 11:14:29 +0800

    发布文章-添加文章

--- src/views/Publish.vue
+++ src/views/Publish.vue
@@ -36,8 +36,8 @@
           <my-channel v-model="articleForm.channel_id"></my-channel>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary">发布文章</el-button>
-          <el-button>存入草稿</el-button>
+          <el-button type="primary" @click="addArticle(false)">发布文章</el-button>
+          <el-button @click="addArticle(true)">存入草稿</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -85,6 +85,19 @@ export default {
         }
       }
     };
+  },
+  methods: {
+    // 添加文章
+    async addArticle(draft) {
+      // draft 参数，false 发布  true 草稿，正好就是后台接口需要的数据
+      try {
+        await this.$http.post(`articles?draft=${draft}`, this.articleForm);
+        this.$message.success(draft ? "存入草稿成功" : "发布文章成功");
+        this.$router.push("/article");
+      } catch (e) {
+        this.$message.error("操作失败");
+      }
+    }
   }
 };
 </script>
