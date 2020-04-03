11d525e5e4327807614d2eafaa8bebf8a36d77fa
unknown <61458694+fengzhuo-FZ@users.noreply.github.com>
2020-04-03 18:58:46 +0800

    个人设置-修改用户信息

--- src/views/Setting.vue
+++ src/views/Setting.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="Setting-container">
+  <div class="setting-container">
     <el-card>
       <div slot="header">
         <my-bread>个人设置</my-bread>
@@ -9,8 +9,8 @@
         <el-col :span="12">
           <!-- 表单 -->
           <el-form label-width="120px">
-            <el-form-item label="编号：">100</el-form-item>
-            <el-form-item label="手机：">13444442222</el-form-item>
+            <el-form-item label="编号：">{{userInfo.id}}</el-form-item>
+            <el-form-item label="手机：">{{userInfo.mobile}}</el-form-item>
             <el-form-item label="媒体名称：">
               <el-input v-model="userInfo.name"></el-input>
             </el-form-item>
@@ -21,7 +21,7 @@
               <el-input v-model="userInfo.email"></el-input>
             </el-form-item>
             <el-form-item>
-              <el-button type="primary">保存设置</el-button>
+              <el-button @click="updateUser()" type="primary">保存设置</el-button>
             </el-form-item>
           </el-form>
         </el-col>
@@ -32,24 +32,20 @@
             action="https://jsonplaceholder.typicode.com/posts/"
             :show-file-list="false"
           >
-            <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+            <img v-if="userInfo.photo" :src="userInfo.photo" class="avatar" />
             <i v-else class="el-icon-plus avatar-uploader-icon"></i>
           </el-upload>
           <p class="edit-photo">修改头像</p>
         </el-col>
       </el-row>
     </el-card>
-    <!-- 测试 -->
-    <com-a></com-a>
-    <com-b></com-b>
   </div>
 </template>
 
 <script>
-import ComA from "@/components/com-a.vue";
-import ComB from "@/components/com-b.vue";
+import eventBus from "@/utils/eventBus";
+import auth from "@/utils/auth";
 export default {
-  components: { ComA, ComB },
   name: "my-setting",
   data() {
     return {
@@ -57,11 +53,40 @@ export default {
       userInfo: {
         name: "",
         intro: "",
-        email: ""
+        email: "",
+        photo: ""
       },
+
       // 支持上传组件的数据
       imageUrl: null
     };
+  },
+  created() {
+    this.getUsetInfo();
+  },
+  methods: {
+    // 修改用户信息
+    async updateUser() {
+      // 准备后台需要的数据
+      const { name, intro, email } = this.userInfo;
+      // 发请求提交
+      await this.$http.patch("user/profile", { name, intro, email });
+      // 成功提示
+      this.$message.success("修改用户信息成功");
+      // 同步Home组件的用户名
+      eventBus.$emit("updateUserName", name);
+      // 同步本地存储的用户名  1获取用户信息  2修改用户信息  3重新存入用户信息
+      const user = auth.getUser();
+      user.name = name;
+      auth.setUser(user);
+    },
+    // 获取用户信息
+    async getUsetInfo() {
+      const {
+        data: { data }
+      } = await this.$http.get("user/profile");
+      this.userInfo = data;
+    }
   }
 };
 </script>
